913a32c7639baec28edac48c287bb4e7b291f195
Jeffrey Davis <j.davis081248@gmail.com>
2023-11-21 18:34:23 -0600

    work?

---
+++ src/indexOG.js
@@ -0,0 +1,88 @@
+const titleElement = document.getElementById("title")
+const yearReleasedElement = document.getElementById("year-released")
+const descriptionElement = document.getElementById("description")
+const movieListElement = document.getElementById("movie-list")
+const detailImageElement = document.getElementById("detail-image")
+const watchedElement = document.getElementById("watched")
+const amountElement = document.getElementById("amount")
+const bloodFormElement = document.getElementById("blood-form")
+const bloodAmountElement = document.getElementById("blood-amount")
+let currentMovie
+let watchStatus
+
+fetch("http://localhost:3000/movies")
+.then(response => response.json())
+.then(movies => {
+    movies.forEach(movie => createNavBar(movie))
+    displayMovie(movies[0])
+})
+
+function createNavBar(movie)
+{
+    const imageElement = document.createElement("img")
+    imageElement.src = movie.image
+    movieListElement.appendChild(imageElement)
+    imageElement.addEventListener("click", () => {displayMovie(movie)})
+}
+
+function displayMovie(movie)
+{
+    currentMovie = movie
+    titleElement.textContent = movie.title
+    yearReleasedElement.textContent = movie.release_year
+    descriptionElement.textContent = movie.description
+    detailImageElement.src = movie.image
+    amountElement.textContent = movie.blood_amount
+    toggleWatchedButton(movie)
+
+    // if (movie.watched === true)
+    // {
+    //     watchedElement.textContent = "Watched"
+    // }
+
+    // else
+    // {
+    //     watchedElement.textContent = "Unwatched"
+    // } 
+}
+
+function toggleWatchedButton(movie)
+{
+    if (movie.watched === true)
+    {
+        watchedElement.textContent = "Watched"
+    }
+
+    else
+    {
+        watchedElement.textContent = "Unwatched"
+    } 
+}
+
+watchedElement.addEventListener("click", () => {
+    if (watchedElement.textContent === "Watched")
+    {
+        watchedElement.textContent = "Unwatched"
+    }
+
+    else
+    {
+        watchedElement.textContent = "Watched"
+    }
+})
+
+bloodFormElement.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    if (isNaN(Number(bloodAmountElement.value))) {
+        alert("Error: That's NOT a number! Please try again.")
+    }
+    
+    else {
+        let newBloodAmount = Number(bloodAmountElement.value) + Number(amountElement.textContent)
+        amountElement.textContent = newBloodAmount
+        console.log(newBloodAmount)
+    }
+
+    bloodFormElement.reset()
+})
